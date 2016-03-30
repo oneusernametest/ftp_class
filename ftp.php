ccafd17330523c45c6a86237ced6a591faecf790
leobonillab <leobonillab@gmail.com>
2016-03-30 17:02:41 -0300

    arregladas fallas al conectar, agregado archivo.txt

--- ftp.php
+++ ftp.php
@@ -24,19 +24,18 @@ class FTP{
  
     // configurar la conexion basica
     $this->connectionId = ftp_connect($this->server);
- 
     // acceder al ftp
     $result = ftp_login($this->connectionId, $this->user, $this->password);
-    $this->logMsgAdd('Intentando conectar al ' . $this->server . ' con el usuario ' . $this->user);
+    $this->logMsgAdd('Intentando conectar al ' . $this->server . ' con el usuario ' . $this->user.'<br/>');
     echo 'conectando ....';
-    // configurar el modo pasivo
+    // configurar el modo activo
     ftp_pasv($this->connectionId, false);
  
     if ((!$this->connectionId) || (!$result)) {
         $this->logMsgAdd('La conexion al ftp ha fallado!');
         return false;
     } else {
-        $this->logMsgAdd('Se ha conectado exitosamente a ' . $this->server . ', con el usuario ' . $this->user);
+        $this->logMsgAdd('Se ha conectado exitosamente a ' . $this->server . ', con el usuario ' . $this->user.'<br/>');
         $this->con_ok = true;
         return true;
     }
@@ -44,25 +43,34 @@ class FTP{
 
  public function deleteFile($file){
  	   if($this->con_ok){
- 	   		ftp_delete($this->connectionId, $file);
+ 	   		if(ftp_delete($this->connectionId, $file)){
+ 	   			$this->logMsgAdd('El archivo '.$file.' fue borrado correctamente<br/>');
+ 	   			 	   		return true;
+ 	   		}else{
+					$this->logMsgAdd('Error borrando el archivo '.$file.' <br/>');
+ 	   		}
+
  	   }else{
- 	   	  $this->logMsgAdd('Error Borrando archivo, No hay conexion activa con el servidor ftp');
-           return false;
+ 	   	  $this->logMsgAdd('Error Borrando archivo, No hay conexion activa con el servidor ftp'.'<br/>');
  	   }
+ 	  return false;
 
  }
 
 
  public function uploadFile($file){
- 	 	   if($this->con_ok){
+ 	 	if($this->con_ok){
  		if (ftp_put($this->connectionId, $this->server, $file, FTP_ASCII)) {
+
+  $this->logMsgAdd('El archivo '.$file.'se ha cargado exitosamente <br />');
+
  			return true;
 		} else {
-			  $this->logMsg('Ha fallado al cargar el archivo');
+			  $this->logMsgAdd('Ha fallado al cargar el archivo <br />');
 
 		}
 	}else{
-		$this->logMsgAdd('Error Cargando archivo ,No hay conexion activa con el servidor ftp');
+		$this->logMsgAdd('Error Cargando archivo ,No hay conexion activa con el servidor ftp <br />');
 	}
 		return false;
  }
@@ -71,46 +79,59 @@ class FTP{
  public function closeConnection(){
  			if($this->con_ok){
  				ftp_close($this->connectionId);
- 			$this->logMsgAdd('La conexion con el servidor ftp se ha cerrado correctamente');
+ 			$this->logMsgAdd('La conexion con el servidor ftp se ha cerrado correctamente <br />');
 	}
  }
 
 
  public function chmodFile($file, $permission){
  		if (ftp_chmod($this->connectionId, $permission, $file) !== false) {
-         $this->logMsg('Actualizado archivo con nuevo permiso'.$permission);
+         $this->logMsgAdd('Actualizado archivo con nuevo permiso'.$permission .'<br />');
          return true;
 } else {
- 		$this->logMsg('No se ha podido actualizar el archivo con permiso '.$permission);
+ 		$this->logMsgAdd('No se ha podido actualizar el archivo con permiso '.$permission .'<br />');
 }
 
  }
 
- public function chmodFolder(){
-
+ public function chmodFolder($folder,$permission){
+	if (ftp_chmod($this->connectionId, $permission, $file) !== false) {
+         $this->logMsgAdd('Actualizado el directorio con nuevo permiso'.$permission .'<br />');
+         return true;
+} else {
+ 		$this->logMsgAdd('No se ha podido actualizar el directorio con permiso '.$permission .'<br />');
+}
  }
 
 
  public function renameFile($old_file, $new_file){
  	if (ftp_rename($this->connectionId, $old_file, $new_file)) {
- 		$this->logMsgAdd('El archivo '.$old_file. '  ha sido  renombrado a '.$new_file);
+ 		$this->logMsgAdd('El archivo '.$old_file. '  ha sido  renombrado a '.$new_file.'<br />');
 } else {
-			$this->logMsgAdd('Hubo un problema al renombrar el archivo');
+			$this->logMsgAdd('Hubo un problema al renombrar el archivo'.'<br />');
 }
 
  }
 
  public function renameFolder($old_folder, $new_folder){
  		if (ftp_rename($this->connectionId, $old_folder, $new_folder)) {
- 		$this->logMsgAdd('El directorio'.$old_folder. '  ha sido  renombrado a '.$new_folder);
+ 		$this->logMsgAdd('El directorio'.$old_folder. '  ha sido  renombrado a '.$new_folder.'<br />');
 } else {
-			$this->logMsgAdd('Hubo un problema al renombrar el directorio');
+			$this->logMsgAdd('Hubo un problema al renombrar el directorio'.'<br />');
 }
 
  }
 
  public function sizeOfFile($file){
+
  		$size = ftp_size($this->connectionId, $file);
+ 		if ($size!= -1) {
+ 			$this->logMsgAdd('El tamaño del archivo '.$file .'es '. $size .' bytes <br />');
+ 			return $size;
+ 		}else{
+ 				$this->logMsgAdd('No se puede determinar el tamaño del archivo'.$file .'es <br />');
+ 		}
+ 		return false;
  }
 public function logMsgAdd($msg){
 		array_push($this->logMsg,$msg);
--- test.php
+++ test.php
@@ -3,7 +3,13 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 require_once('ftp.php');
+$file = 'archivo.txt';
+$file_remote = 'remote.txt';
 
 $myFtp = new FTP('31.220.16.118','u303615875','123456');
 $myFtp->connect();
-//$myFtp->showLog();
+//$myFtp->uploadFile($file);
+//$myFtp->deleteFile($file);
+echo $myFtp->sizeOfFile($file_remote);
+$myFtp->closeConnection();
+$myFtp->showLog();
