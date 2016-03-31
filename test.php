2ea2fbe413102dc228cd2fec6deb65c5ebb53e68
leobonillab <leobonillab@gmail.com>
2016-03-31 15:52:17 -0300

    ya no es necesario el metodo connect()

--- test.php
+++ test.php
@@ -1,14 +1,14 @@
 <?php 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+//error_reporting(E_ALL);
+//ini_set('display_errors', 1);
 
 require_once('ftp.php');
 $file = 'archivo.txt';
 $file_remote = 'remote.txt';
 
 $myFtp = new FTP('31.220.16.118','u303615875','123456');
-$myFtp->connect();
-//$myFtp->uploadFile($file);
+$myFtp->uploadFile($file);
+$myFtp->renameFile($file,'nuevo.txt');
 //$myFtp->deleteFile($file);
 echo $myFtp->sizeOfFile($file_remote);
 $myFtp->closeConnection();
